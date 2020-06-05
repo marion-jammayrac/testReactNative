43859ef12751df58bee97a9bf480221199dd5edc
mario <marion.jammayrac@isen.yncrea.fr>
2020-06-05 09:23:00 +0200

    test 1

---
+++ Components/Search.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
+import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi' 
+
+class Search extends React.Component {
+
+  _loadFilms() {
+    if (this.searchedText.length > 0) {
+      this.setState({ isLoading: true }) // Lancement du chargement
+      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+          this.setState({ 
+            films: data.results,
+            isLoading: false // Arrêt du chargement
+          })
+      })
+    }
+}
+
+   constructor(props) {
+    super(props)
+    this.state = { 
+      isLoading: false, //att lancement
+      films: [] }
+  }
+
+  _searchTextInputChanged(text) {
+    this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+  }
+
+  _displayLoading() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.loading_container}>
+          <ActivityIndicator size='large' />
+          {/* Le component ActivityIndicator possède une propriété size pour définir la taille du visuel de chargement : small ou large. Par défaut size vaut small, on met donc large pour que le chargement soit bien visible */}
+        </View>
+      )
+    }
+  }
+
+  _displayDetailForFilm = (idFilm) => {
+    console.log("Display film with id " + idFilm)
+    this.props.navigation.navigate("FilmDetail", { idFilm: idFilm })
+}
+
+  render() {
+    console.log(this.props)
+    return (
+      <View style={styles.main_container}>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._loadFilms()} //ENTRE LOAD !
+        />
+        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+            {/* Ici j'ai simplement repris l'exemple sur la documentation de la FlatList */}
+            <FlatList
+            data={this.state.films}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({item}) => <FilmItem film={item} displayDetailForFilm={this._displayDetailForFilm} 
+            />}
+            />
+             {this._displayLoading()}
+          </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  },
+  loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 100,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default Search
---
+++ Navigation/Navigation.js
@@ -0,0 +1,20 @@
+import { createStackNavigator, } from 'react-navigation-stack'
+import {createAppContainer } from 'react-navigation'
+import Search from '../Components/Search'
+import FilmDetail from '../Components/FilmDetails'
+
+
+const SearchStackNavigator = createStackNavigator({
+  Search: { 
+       screen: Search,
+    navigationOptions: {
+      title: 'Rechercher'
+    }
+    },
+    FilmDetail: {
+        screen: FilmDetail
+    }
+})
+
+
+export default createAppContainer(SearchStackNavigator)
